e41614d180783ae7f547d95b8c152b82442632ca
Fabio Castro Morffi <fabiocastromorffi@gmail.com>
2025-03-24 14:29:40 -0400

    whatev

--- week-7/euler_method.py
+++ week-7/euler_method.py
@@ -1,18 +1,33 @@
-m = 0.003
-A = 150
-p = 1.22
-C = 0.5
-def a_y(v):
-    return -9.8 + ((C*p*A*v**2)/ 2*m)
-vi = 0
-vf = 0
-t = 0.2
-lst = []
-for i in range(1, 10):
-    a = a_y(vi)
-    vf = vi + a*t
-    vi = vf
-    lst.append((vf,t*i))
+import matplotlib.pyplot as plt
+import pandas as pd
 
-print(lst)
-print("h")
+#df = pd.read_csv("clean_data_multiple_filter.csv")
+mass = 0.003
+area = 190
+air_density = 1.22
+drag_coeff = 0.5
+def newAcc(v):
+    return -9.8 + ((drag_coeff*air_density*area*v**2)/ 2*mass)
+initial_velocity, final_velocity = 0, 0
+initial_position = 1
+delta_t = 0.002
+#lst final_velocity = []
+lst_time = []
+lst_position = []
+#acc = -9.8
+while initial_position>0:
+    acc = newAcc(initial_velocity)
+    #print(acc)
+    final_velocity = initial_velocity + acc*0.2
+    final_position = initial_position + initial_velocity*0.002 + 0.5*acc*0.002**2 
+    #print(final_position)
+    initial_velocity, initial_position = final_velocity, final_position
+    delta_t+=0.002
+    #lst final_velocity.append final_velocity)
+    lst_time.append(delta_t)
+    lst_position.append(final_position)
+
+
+plt.plot(lst_time, lst_position)
+#plt.plot(lst_time, lst final_velocity)
+plt.show()
