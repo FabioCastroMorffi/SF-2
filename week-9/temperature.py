30cac356661155c398693ee74eb3da376f01fb70
FabioCastroMorffi <141592488+FabioCastroMorffi@users.noreply.github.com>
2025-04-07 15:46:44 -0400

    Add files via upload

---
+++ week-9/temperature.py
@@ -0,0 +1,53 @@
+#Fabio Castro Morffi 2438078
+
+#Start of the assignment
+
+def toCelsius(temp_F):
+    return round(((temp_F - 32) * (5/9)),2) 
+
+def avgTempYear(dict, year):
+    try:
+        existing_year = dict[year]
+    except KeyError:
+        print("year not in dic")
+    else:
+        return (round(sum(existing_year)/len(existing_year),2))
+    
+def topThreeYears(dict):
+    lst = []
+    removal_set = set()
+    for key in dict:
+        curr_avg = avgTempYear(dict, key)
+        removal_set.add(curr_avg)
+    for i in range(3):
+        maxes = max(removal_set)
+        removal_set.remove(maxes)
+        lst.append(maxes)
+    return lst
+
+        
+    
+def avgTempMonth(dict, month):
+    avg_temp = []
+    month_dict = {'JAN': 1, 'FEB': 2, 'MAR': 3, 'APR': 4, 'MAY': 5, 'JUN': 6, 'JUL':7, 'AUG': 8,'SEP':9 ,'OCT':10,'NOV':10,'DEC':12}
+    for value in dict:
+        avg_temp.append(dict[value][month_dict[month] -1])
+    return round(sum(avg_temp)/len(avg_temp),2)
+
+
+
+def main():
+    input_file = open('data.txt', 'r')
+    temp_dict = {}
+    #lst_lines = input_file.readlines()
+    for line in input_file:
+        if line[0][0] == "1":
+            line = line.rstrip().split()
+            temp = list(map(float, line[1:]))
+            temp_dict[int(line[0])] = list(map(toCelsius, temp))
+    #print(temp_dict)
+    #print(avgTempMonth(temp_dict, "JAN"))
+    input_file.close()
+    
+
+main()
