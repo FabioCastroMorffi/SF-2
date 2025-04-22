759bff3b612b132ece384e0807854b6f8c7294de
Fabio <fabiocastromorffi@gmail.com>
2025-04-22 18:40:10 -0400

    fixed and added some stuff

--- week-9/physics_lab.py
+++ week-9/physics_lab.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 df = pd.read_csv("clean_data_multiple_filter.csv")
 
-real_time = df["Time (s) Run #4"]  
+real_time = df["Time (s) Run #4"]
 real_position = df["Position (m) Run #4"]
 initial_position = real_position.dropna().iloc[0] 
 
@@ -12,11 +12,11 @@ area = 0.019
 air_density = 1.2
 drag_coeff = 0.97
 def newAcc(v):
-    return -9.8 + ((drag_coeff*air_density*area*v**2)/ 2*mass)
+    return -9.8 + ((drag_coeff*air_density*area*v**2)/ (2*mass)) 
 
-initial_velocity, final_velocity = 0, 0
-delta_t = -0.02
-
+initial_velocity, final_velocity = (real_position.iloc[147] - real_position.iloc[146]) / (real_time.iloc[1] - real_time.iloc[0]), 0
+running_time = -0.02
+dt = 0.02
 lst_final_velocity = []
 lst_time = []
 lst_position = []
@@ -25,14 +25,14 @@ lst_position = []
 while initial_position>0:
     acc = newAcc(initial_velocity)
 
-    final_velocity = initial_velocity + acc*0.02
-    final_position = initial_position + initial_velocity*0.02 + 0.5*acc*0.02**2 
+    final_velocity = initial_velocity + acc*dt
+    final_position = initial_position + initial_velocity*dt + 0.5*acc*dt**2 
 
 
     initial_velocity, initial_position = final_velocity, final_position
-    delta_t+=0.02
+    running_time+=dt
     lst_final_velocity.append(final_velocity)
-    lst_time.append(delta_t)
+    lst_time.append(running_time)
     lst_position.append(final_position)
 
 
