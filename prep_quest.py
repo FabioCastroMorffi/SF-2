29d3d4967c8e90915988f337667c13d4a2cc764b
Fabio Castro Morffi <fabiocastromorffi@gmail.com>
2025-05-10 11:55:41 -0400

    dale

---
+++ prep_quest.py
@@ -0,0 +1,4 @@
+#Longest Common Prefix
+
+def recursiveLCP(lst:list[str],curr_lcp:str):
+    
---
+++ week-14/recursion/consonants_rec.py
@@ -0,0 +1,5 @@
+def recursiveConsonants(string:str,count =0):
+    if len(string) == 0:
+        return 0
+    if string[0].isVowel()
+    return recursiveConsonants(string[1:]) + count
